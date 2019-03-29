b78c149dd867d1be1bb3966b2a228b638f113ab6
wood </>
2019-03-28 21:55:15 -0700

    feat: add the parallel functionality

--- bin/jasmine.js
+++ bin/jasmine.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 
-var path = require('path'),
-    Command = require('../lib/command.js'),
-    Jasmine = require('../lib/jasmine.js');
+var cluster = require('cluster'),
+    path = require('path'),
+    Jasmine = require("../lib/jasmine");
 
 var jasmine = new Jasmine({ projectBaseDir: path.resolve() });
-var examplesDir = path.join(path.dirname(require.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
-var command = new Command(path.resolve(), examplesDir, console.log);
 
-command.run(jasmine, process.argv.slice(2));
+if (cluster.isMaster) {
+  var Command = require('../lib/command.js');
+  
+  var examplesDir = path.join(path.dirname(require.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
+  var command = new Command(path.resolve(), examplesDir, console.log);
+
+  command.run(jasmine, process.argv.slice(2));
+} else if (cluster.isWorker) {
+  var loadConfig = require('../lib/loadConfig');
+  var runWorkerJasmine = require('../lib/worker');
+  runWorkerJasmine(jasmine, loadConfig);
+}
