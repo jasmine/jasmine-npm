4b3938996d41b2916dcbc0e6485fbaed35d14f3d
Gregg Van Hove <gvanhove@pivotal.io>
2020-09-28 17:35:12 -0700

    Remove code related to multiple workers.
    - This functionality doesn't appear to have ever worked. It looks like
      there are some changes necessary to jasmine-core to be able to support
      multiple workers, but the original submitter didn't actually try this
      out
    
    - See #153
    - Fixes #163
    - Fixes #165
    - See also discussion in #162

--- bin/jasmine.js
+++ bin/jasmine.js
@@ -1,20 +1,11 @@
 #!/usr/bin/env node
 
-var cluster = require('cluster'),
-    path = require('path'),
-    Jasmine = require("../lib/jasmine");
+var path = require('path'),
+    Command = require('../lib/command'),
+    Jasmine = require('../lib/jasmine');
 
 var jasmine = new Jasmine({ projectBaseDir: path.resolve() });
+var examplesDir = path.join(path.dirname(require.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
+var command = new Command(path.resolve(), examplesDir, console.log);
 
-if (cluster.isMaster) {
-  var Command = require('../lib/command.js');
-  
-  var examplesDir = path.join(path.dirname(require.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
-  var command = new Command(path.resolve(), examplesDir, console.log);
-
-  command.run(jasmine, process.argv.slice(2));
-} else if (cluster.isWorker) {
-  var loadConfig = require('../lib/loadConfig');
-  var runWorkerJasmine = require('../lib/worker');
-  runWorkerJasmine(jasmine, loadConfig);
-}
+command.run(jasmine, process.argv.slice(2));
