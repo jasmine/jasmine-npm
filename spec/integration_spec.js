da3ecaa3873286ed90cf1407763d0bea876b219d
Steve Gravrock <sdg@panix.com>
2021-05-03 12:37:36 -0700

    Opt-in support for ES modules with .js extension and package type `module`
    * All files are loaded using `import` if `"jsLoader": "import"` is set in
      the config file.
    * Only supported on Node >= 12.17.0. Older versions have missing or broken
      support for importing .js files.
    * Fixes #170

--- spec/integration_spec.js
+++ spec/integration_spec.js
@@ -1,8 +1,29 @@
 const child_process = require('child_process');
 
 describe('Integration', function () {
+  beforeEach(function() {
+    jasmine.addMatchers({
+      toBeSuccess: function(matchersUtil) {
+        return {
+          compare: function(actual, expected) {
+            const result = { pass: actual.exitCode === 0 };
+
+            if (result.pass) {
+              result.message = 'Expected process not to succeed but it did.';
+            } else {
+              result.message = `Expected process to succeed but it exited ${actual.exitCode}.`;
+            }
+
+            result.message += '\n\nOutput:\n' + actual.output;
+            return result;
+          }
+        };
+      }
+    });
+  });
+
   it('supports ES modules', async function () {
-    let {exitCode, output} = await runJasmine('spec/fixtures/esm');
+    let {exitCode, output} = await runJasmine('spec/fixtures/esm', true);
     expect(exitCode).toEqual(0);
     // Node < 14 outputs a warning when ES modules are used, e.g.:
     // (node:5258) ExperimentalWarning: The ESM module loader is experimental.
@@ -20,15 +41,35 @@ describe('Integration', function () {
     );
   });
 
+  it('loads .js files using import when jsLoader is "import"', async function() {
+    await requireFunctioningJsImport();
+    expect(await runJasmine('spec/fixtures/js-loader-import', false)).toBeSuccess();
+  });
+
+  it('warns that jsLoader: "import" is not supported', async function() {
+    await requireBrokenJsImport();
+    const {output} = await runJasmine('spec/fixtures/js-loader-import', false);
+    expect(output).toContain('Warning: jsLoader: "import" may not work ' +
+      'reliably on Node versions before 12.17.');
+  });
+
+  it('loads .js files using require when jsLoader is "require"', async function() {
+    expect(await runJasmine('spec/fixtures/js-loader-require', false)).toBeSuccess();
+  });
+
+  it('loads .js files using require when jsLoader is undefined', async function() {
+    expect(await runJasmine('spec/fixtures/js-loader-default', false)).toBeSuccess();
+  });
+
   it('handles load-time exceptions from CommonJS specs properly', async function () {
-    const {exitCode, output} = await runJasmine('spec/fixtures/cjs-load-exception');
+    const {exitCode, output} = await runJasmine('spec/fixtures/cjs-load-exception', false);
     expect(exitCode).toEqual(1);
     expect(output).toContain('Error: nope');
     expect(output).toMatch(/at .*throws_on_load.js/);
   });
 
   it('handles load-time exceptions from ESM specs properly', async function () {
-    const {exitCode, output} = await runJasmine('spec/fixtures/esm-load-exception');
+    const {exitCode, output} = await runJasmine('spec/fixtures/esm-load-exception', true);
     expect(exitCode).toEqual(1);
     expect(output).toContain('Error: nope');
     expect(output).toMatch(/at .*throws_on_load.mjs/);
@@ -42,18 +83,24 @@ describe('Integration', function () {
   });
 
   it('handles syntax errors in ESM specs properly', async function () {
-    const {exitCode, output} = await runJasmine('spec/fixtures/esm-syntax-error');
+    const {exitCode, output} = await runJasmine('spec/fixtures/esm-syntax-error', true);
     expect(exitCode).toEqual(1);
     expect(output).toContain('SyntaxError');
     expect(output).toContain('syntax_error.mjs');
   });
 });
 
-async function runJasmine(cwd) {
+async function runJasmine(cwd, useExperimentalModulesFlag) {
   return new Promise(function(resolve) {
+    const args = ['../../../bin/jasmine.js', '--config=jasmine.json'];
+
+    if (useExperimentalModulesFlag) {
+      args.unshift('--experimental-modules');
+    }
+
     const child = child_process.spawn(
       'node',
-      ['--experimental-modules', '../../../bin/jasmine.js', '--config=jasmine.json'],
+      args,
       {
         cwd,
         shell: false
@@ -70,4 +117,29 @@ async function runJasmine(cwd) {
       resolve({exitCode, output});
     });
   });
+}
+
+async function requireFunctioningJsImport() {
+  if (!(await hasFunctioningJsImport())) {
+    pending("This Node version can't import .js files");
+  }
+}
+
+async function requireBrokenJsImport() {
+  if (await hasFunctioningJsImport()) {
+    pending("This Node version can import .js files");
+  }
+}
+
+async function hasFunctioningJsImport() {
+  try {
+    await import('./fixtures/js-loader-import/anEsModule.js');
+    return true;
+  } catch (e) {
+    if (e.code === 'ERR_MODULE_NOT_FOUND') {
+      throw e;
+    }
+
+    return false;
+  }
 }
--- spec/loader_spec.js
+++ spec/loader_spec.js
@@ -6,72 +6,88 @@ describe('loader', function() {
   });
 
   describe('#load', function() {
-    describe('When the path ends in .mjs', function () {
-      it('loads the file as an es module', async function () {
-        const requireShim = jasmine.createSpy('requireShim');
-        let resolve;
-        const importPromise = new Promise(function (res) {
-          resolve = res;
-        });
-        const importShim = jasmine.createSpy('importShim')
-          .and.returnValue(importPromise);
-        const loader = new Loader({requireShim, importShim});
-
-        const loaderPromise = loader.load('./foo/bar/baz.mjs');
-
-        expect(requireShim).not.toHaveBeenCalled();
-        expect(importShim).toHaveBeenCalledWith('file://./foo/bar/baz.mjs');
-        await expectAsync(loaderPromise).toBePending();
-
-        resolve();
-
-        await expectAsync(loaderPromise).toBeResolved();
+    describe('With alwaysImport: true', function() {
+      describe('When the path ends in .mjs', function () {
+        esModuleSharedExamples('mjs', true);
       });
 
-      it("adds the filename to errors that don't include it", async function() {
-        const underlyingError = new SyntaxError('some details but no filename, not even in the stack trace');
-        const importShim = () => Promise.reject(underlyingError);
-        const loader = new Loader({importShim});
-
-        await expectAsync(loader.load('foo.mjs')).toBeRejectedWithError(
-          "While loading foo.mjs: SyntaxError: some details but no filename, not even in the stack trace"
-        );
-      });
-
-      it('propagates errors that already contain the filename without modifying them', async function () {
-        const requireShim = jasmine.createSpy('requireShim');
-        const underlyingError = new Error('nope');
-        underlyingError.stack = underlyingError.stack.replace('loader_spec.js', 'foo.mjs');
-        const importShim = jasmine.createSpy('importShim')
-          .and.callFake(() => Promise.reject(underlyingError));
-        const loader = new Loader({requireShim, importShim});
-
-        await expectAsync(loader.load('foo.mjs')).toBeRejectedWith(underlyingError);
+      describe('When the path does not end in .mjs', function () {
+        esModuleSharedExamples('js', true);
       });
     });
 
-    describe('When the path does not end in .mjs', function () {
-      it('loads the file as a commonjs module', async function () {
-        const requireShim = jasmine.createSpy('requireShim')
-          .and.returnValue(Promise.resolve());
-        const importShim = jasmine.createSpy('importShim');
-        const loader = new Loader({requireShim, importShim});
-
-        await expectAsync(loader.load('./foo/bar/baz')).toBeResolved();
-
-        expect(requireShim).toHaveBeenCalledWith('./foo/bar/baz');
-        expect(importShim).not.toHaveBeenCalled();
+    describe('With alwaysImport: false', function() {
+      describe('When the path ends in .mjs', function () {
+        esModuleSharedExamples('mjs', false);
       });
 
-      it('propagates the error when import fails', async function () {
-        const underlyingError = new Error('nope');
-        const requireShim = jasmine.createSpy('requireShim')
-          .and.throwError(underlyingError);
-        const importShim = jasmine.createSpy('importShim');
-        const loader = new Loader({requireShim, importShim});
+      describe('When the path does not end in .mjs', function () {
+        it('loads the file as a commonjs module', async function () {
+          const requireShim = jasmine.createSpy('requireShim')
+            .and.returnValue(Promise.resolve());
+          const importShim = jasmine.createSpy('importShim');
+          const loader = new Loader({requireShim, importShim});
 
-        await expectAsync(loader.load('foo')).toBeRejectedWith(underlyingError);
+          await expectAsync(loader.load('./foo/bar/baz', false)).toBeResolved();
+
+          expect(requireShim).toHaveBeenCalledWith('./foo/bar/baz');
+          expect(importShim).not.toHaveBeenCalled();
+        });
+
+        it('propagates the error when import fails', async function () {
+          const underlyingError = new Error('nope');
+          const requireShim = jasmine.createSpy('requireShim')
+            .and.throwError(underlyingError);
+          const importShim = jasmine.createSpy('importShim');
+          const loader = new Loader({requireShim, importShim}, false);
+
+          await expectAsync(loader.load('foo')).toBeRejectedWith(underlyingError);
+        });
       });
     });
   });
 });
+
+function esModuleSharedExamples(extension, alwaysImport) {
+  it('loads the file as an es module', async function () {
+    const requireShim = jasmine.createSpy('requireShim');
+    let resolve;
+    const importPromise = new Promise(function (res) {
+      resolve = res;
+    });
+    const importShim = jasmine.createSpy('importShim')
+      .and.returnValue(importPromise);
+    const loader = new Loader({requireShim, importShim});
+
+    const loaderPromise = loader.load(`./foo/bar/baz.${extension}`, alwaysImport);
+
+    expect(requireShim).not.toHaveBeenCalled();
+    expect(importShim).toHaveBeenCalledWith(`file://./foo/bar/baz.${extension}`);
+    await expectAsync(loaderPromise).toBePending();
+
+    resolve();
+
+    await expectAsync(loaderPromise).toBeResolved();
+  });
+
+  it("adds the filename to errors that don't include it", async function() {
+    const underlyingError = new SyntaxError('some details but no filename, not even in the stack trace');
+    const importShim = () => Promise.reject(underlyingError);
+    const loader = new Loader({importShim});
+
+    await expectAsync(loader.load(`foo.${extension}`, alwaysImport)).toBeRejectedWithError(
+      `While loading foo.${extension}: SyntaxError: some details but no filename, not even in the stack trace`
+    );
+  });
+
+  it('propagates errors that already contain the filename without modifying them', async function () {
+    const requireShim = jasmine.createSpy('requireShim');
+    const underlyingError = new Error('nope');
+    underlyingError.stack = underlyingError.stack.replace('loader_spec.js', `foo.${extension}`);
+    const importShim = jasmine.createSpy('importShim')
+      .and.callFake(() => Promise.reject(underlyingError));
+    const loader = new Loader({requireShim, importShim});
+
+    await expectAsync(loader.load(`foo.${extension}`, alwaysImport)).toBeRejectedWith(underlyingError);
+  });
+}
