jasmine/jasmine-npm

commit e2a93805a88eb4b15c012c08ea030811c4fdb0dc

Fixed loading of external reporters in the default configuration
* Fixes #189

lib/loader.js
@@ -11,11 +11,18 @@ function Loader(options) {
 
 Loader.prototype.load = function(modulePath) {
   if ((this.alwaysImport && !modulePath.endsWith('.json')) || modulePath.endsWith('.mjs')) {
-    // The ES module spec requires import paths to be valid URLs. As of v14,
-    // Node enforces this on Windows but not on other OSes. On OS X, import
-    // paths that are URLs must not contain parent directory references.
-    const url = `file://${this.resolvePath_(modulePath)}`;
-    return this.import_(url)
+    let importSpecifier;
+
+    if (modulePath.indexOf('/') === -1) {
+      importSpecifier = modulePath;
+    } else {
+      // The ES module spec requires import paths to be valid URLs. As of v14,
+      // Node enforces this on Windows but not on other OSes. On OS X, import
+      // paths that are URLs must not contain parent directory references.
+      importSpecifier = `file://${this.resolvePath_(modulePath)}`;
+    }
+
+    return this.import_(importSpecifier)
       .then(
         mod => mod.default,
         e => {

spec/loader_spec.js
@@ -39,6 +39,21 @@ describe('loader', function() {
         });
       });
 
+      it('imports non-local modules', async function() {
+        const payload = {default: {}};
+        const requireShim = jasmine.createSpy('requireShim');
+        const importShim = jasmine.createSpy('importShim')
+          .and.returnValue(Promise.resolve(payload));
+        const loader = new Loader({requireShim, importShim});
+        loader.alwaysImport = true;
+
+        const result = await loader.load('some-module');
+
+        expect(result).toBe(payload.default);
+        expect(requireShim).not.toHaveBeenCalled();
+        expect(importShim).toHaveBeenCalledWith('some-module');
+      });
+
       it('uses require to load JSON files', async function() {
         const requireShim = jasmine.createSpy('requireShim')
           .and.returnValue(Promise.resolve());

commit 9b331b68e3d65eae93e6f748b0912c74576f1f62

Fixed module loading on Windows (broken in e2a9380)

lib/loader.js
@@ -13,7 +13,7 @@ Loader.prototype.load = function(modulePath) {
   if ((this.alwaysImport && !modulePath.endsWith('.json')) || modulePath.endsWith('.mjs')) {
     let importSpecifier;
 
-    if (modulePath.indexOf('/') === -1) {
+    if (modulePath.indexOf(path.sep) === -1 && modulePath.indexOf('/') === -1) {
       importSpecifier = modulePath;
     } else {
       // The ES module spec requires import paths to be valid URLs. As of v14,

spec/loader_spec.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const Loader = require('../lib/loader');
 
 describe('loader', function() {
@@ -116,7 +117,7 @@ describe('loader', function() {
 });
 
 function esModuleSharedExamples(extension, alwaysImport) {
-  it('loads the file as an es module', async function () {
+  async function testBasicEsModuleLoading(separator) {
     const requireShim = jasmine.createSpy('requireShim');
     let resolve;
     const importPromise = new Promise(function (res) {
@@ -129,16 +130,25 @@ function esModuleSharedExamples(extension, alwaysImport) {
     const loader = new Loader({requireShim, importShim, resolvePath});
     loader.alwaysImport = alwaysImport;
 
-    const loaderPromise = loader.load(`./foo/bar/baz.${extension}`);
+    const requestedPath = ['foo', 'bar', `baz.${extension}`].join(separator);
+    const loaderPromise = loader.load(requestedPath);
 
     expect(requireShim).not.toHaveBeenCalled();
-    expect(resolvePath).toHaveBeenCalledWith(`./foo/bar/baz.${extension}`);
+    expect(resolvePath).toHaveBeenCalledWith(requestedPath);
     expect(importShim).toHaveBeenCalledWith('file:///the/path/to/the/module');
     await expectAsync(loaderPromise).toBePending();
 
     resolve({});
 
     await expectAsync(loaderPromise).toBeResolved();
+  }
+  
+  it('loads the file as an es module', async function () {
+    await testBasicEsModuleLoading(path.sep);
+  });
+  
+  it('supports /-separated paths', async function() {
+    await testBasicEsModuleLoading('/');
   });
 
   it("adds the filename to ES module syntax errors", async function() {